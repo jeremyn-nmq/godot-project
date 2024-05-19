2f47277a0deffcec229f417cb04c32f6d6d5661e
quinndiep <diepphuongquynh.dev@gmail.com>
2024-05-19 12:41:34 +0700

    update api call

--- mesh_map.gd
+++ mesh_map.gd
@@ -14,25 +14,7 @@ func _ready():
 
 
 func generate_terrain():
-	#var myArray = [
-		#[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-		#[0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0], 
-		#[0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 0], 
-		#[0, 2, 2, 0, 2, 2, 0, 0, 2, 3, 0], 
-		#[0, 2, 2, 0, 3, 2, 0, 0, 2, 2, 0], 
-		#[0, 2, 2, 0, 2, 2, 0, 0, 2, 2, 0], 
-		#[0, 2, 2, 0, 2, 3, 0, 0, 2, 2, 0], 
-		#[0, 2, 2, 0, 2, 2, 0, 0, 3, 2, 0], 
-		#[0, 4, 2, 0, 2, 2, 0, 0, 3, 2, 0], 
-		#[0, 0, 0, 0, 2, 3, 0, 0, 2, 2, 0], 
-		#[0, 2, 3, 0, 0, 0, 0, 0, 3, 0, 0], 
-		#[0, 2, 2, 0, 2, 3, 0, 0, 2, 3, 0], 
-		#[0, 2, 3, 0, 3, 2, 0, 0, 3, 2, 0], 
-		#[0, 2, 2, 0, 2, 2, 0, 0, 2, 2, 0], 
-		#[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	#]
-	
-	var myArray = [
+	var localData = [
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
 		[0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0], 
 		[0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 0], 
@@ -51,27 +33,27 @@ func generate_terrain():
 	]
 	
 	if(Global.data):
-		myArray = Global.data
+		localData = Global.data
 		
 	var a_mesh:ArrayMesh
 	var surftool = SurfaceTool.new()
 	surftool.begin(Mesh.PRIMITIVE_TRIANGLES)
-	for z in range(myArray.size() + 1):
-		for x in range(myArray[0].size() + 1):
+	for z in range(localData.size() + 1):
+		for x in range(localData[0].size() + 1):
 			var y = 0
 			surftool.add_vertex(Vector3(x,y,z))
-			if(myArray[z-1][x-1] == 1):
+			if(localData[z-1][x-1] == 1):
 				draw_box(Vector3(x,y+0.5,z))
-			if(myArray[z-1][x-1] == 2):
+			if(localData[z-1][x-1] == 2):
 				draw_ocupied_lot(Vector3(x,y+0.5,z))
-			if(myArray[z-1][x-1] == 3):
+			if(localData[z-1][x-1] == 3):
 				draw_empty_lot(Vector3(x,y+0.1,z))
-			if(myArray[z-1][x-1] == 4):
+			if(localData[z-1][x-1] == 4):
 				draw_neareast_empty_lot(Vector3(x,y+0.1,z))	
 				target = Vector3(x,y,z)
 	
-	var zSize = myArray.size()
-	var xSize = myArray[0].size()
+	var zSize = localData.size()
+	var xSize = localData[0].size()
 	var vert = 0
 	for z in range(zSize):
 		for x in range(xSize):
@@ -136,26 +118,7 @@ func draw_neareast_empty_lot(pos: Vector3):
 	ins.mesh = box
 	var material = StandardMaterial3D.new()
 	material.albedo_color = Color(255, 246, 0, 1)
-	ins.mesh.surface_set_material(0, material)
-#func draw_map(x,z):
-	#var myArray = [
-		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
-		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
-		#[0,1,1,1,0,0,1,1,1,0,0,1,1,1,0],
-		#[0,2,1,2,0,0,2,1,2,0,0,2,1,2,0],
-		#[0,4,1,2,0,0,2,1,2,0,0,2,1,2,0],
-		#[0,2,1,3,0,0,2,1,3,0,0,2,1,2,0],
-		#[0,2,1,2,0,0,2,1,2,0,0,2,1,2,0],
-		#[0,2,1,2,0,0,2,1,2,0,0,3,1,2,0],
-		#[0,2,1,3,0,0,2,1,3,0,0,2,1,2,0],
-		#[0,2,1,2,0,0,2,1,2,0,0,2,1,2,0],
-		#[0,1,1,1,0,0,1,1,1,0,0,1,1,1,0],
-		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-		#[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	#];
-
-			
-			
+	ins.mesh.surface_set_material(0, material)			
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
